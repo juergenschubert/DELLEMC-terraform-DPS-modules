c985240fd38e12573199cf86f1288a5df667f7f0
Juergen Schubert <juergen.schubert@web.de>
2021-03-12 14:13:41 +0100

    update

---
+++ modules/aws/ddve/ver6/aws_instance-v2/aws_instance-v2.tf
@@ -0,0 +1,34 @@
+
+resource "aws_instance" "terraform_ddve" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  # subnet the instance runs into
+  subnet_id = var.aws-subnet-id
+  # key name
+  key_name = var.key_name
+
+  # Security group assign to instance
+  vpc_security_group_ids = [var.security_group_id]
+  # tighten things up with the instance profile
+  iam_instance_profile = var.iam_instance_profile
+  ### delete associated ec2 disks
+  root_block_device {
+    delete_on_termination = true
+  }
+  tags = {
+    Name = var.ddve_name
+  }
+}
+
+output "public_ipv4" {
+    value = aws_instance.terraform_ddve.public_ip
+}
+output "ec2_id" {
+     value = aws_instance.terraform_ddve.id
+}
+output "public_v4_ip" {
+  value = aws_instance.terraform_ddve.public_ip
+}
+output "public_dns_name" {
+  value = aws_instance.terraform_ddve.public_dns
+}
---
+++ modules/aws/ddve/ver6/aws_instance-v2/variable.tf
@@ -0,0 +1,8 @@
+variable "ddve_name" {}
+variable "ami_id" {}
+variable "region" {}
+variable "instance_type" {}
+variable "aws-subnet-id" {}
+variable "key_name" {}
+variable "iam_instance_profile" {}
+variable "security_group_id" {}
