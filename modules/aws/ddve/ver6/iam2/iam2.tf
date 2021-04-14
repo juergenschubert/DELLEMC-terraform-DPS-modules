9d3ad327fa1eabbd761c58480123324359b707e6
Juergen Schubert <juergen.schubert@web.de>
2021-04-14 17:18:23 +0200

    latest and greatest

--- modules/aws/ddve/ver6/iam2/iam2.tf
+++ modules/aws/ddve/ver6/iam2/iam2.tf
@@ -1,12 +1,3 @@
-variable "iam_role_name" {
-  default = "iam_role_ddve6_terraform"
-}
-variable "iam_policy_name" {
-  default = "s3_ddve_iam_policy_ddve6_terraform"
-}
-variable "instance_profile_name" {
-  default = "instance_profile_terraform"
-}
 ##
 ### IAM Role
 ##
---
+++ modules/aws/ddve/ver6/iam2/variable.tf
@@ -0,0 +1,11 @@
+variable "iam_role_name" {
+  default = "iam_role_ddve6_terraform"
+}
+variable "iam_policy_name" {
+  default = "s3_ddve_iam_policy_ddve6_terraform"
+}
+variable "instance_profile_name" {
+  default = "instance_profile_terraform"
+}
+variable "bucket_name" {}
+
